1e27c4133abe99a1826ff290e41904d32b7d5fb2
EnzoFelyx <enzofelyx@gmail.com>
2026-09-24 15:29:28 -0300

    fix: load fonts error

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,17 +7,31 @@ import { Baloo2_600SemiBold } from '@expo-google-fonts/baloo-2/600SemiBold';
 import { Baloo2_700Bold } from '@expo-google-fonts/baloo-2/700Bold';
 import { Baloo2_800ExtraBold } from '@expo-google-fonts/baloo-2/800ExtraBold';
 import { useFonts } from '@expo-google-fonts/baloo-2/useFonts';
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
 
-  let [fontsLoaded] = useFonts({
+  let [fontsLoaded, fontError] = useFonts({
     Baloo2_400Regular,
     Baloo2_500Medium,
     Baloo2_600SemiBold,
     Baloo2_700Bold,
     Baloo2_800ExtraBold
   });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
